cffbb87f9c12a46c4861d7fc6d02fee25d42808f
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2022-08-24 19:58:01 +0900

    Modify js_main.py files
    2022.08.24

--- loadWebsite/websiteMain/js_main.py
+++ loadWebsite/websiteMain/js_main.py
@@ -5,8 +5,8 @@ window.addEventListener('load', function() {
     var startTestButton = parent.document.getElementsByClassName("u-active-palette-1-light-1 u-border-none u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-6 u-text-active-white u-text-body-alt-color u-text-hover-white u-btn-1")[0];
     
     startTestButton.addEventListener("click", ()=> {
-        document.querySelector("section").scrollTo(0, 0);
-        document.querySelectorAll(".stButton > button")[5].click();
+        parent.document.querySelector("section").scrollTo(0, 0);
+        parent.document.querySelectorAll(".stButton > button")[5].click();
     });
 })
 </script>"""
